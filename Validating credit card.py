231d2aa58946e95f0ecffc891c4f25cb11de7ec5
Meenatchisundari <85951411+Meenatchisundari@users.noreply.github.com>
2022-01-23 18:49:30 +0530

    Add files via upload

---
+++ Validating credit card.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+import re
+
+def check(card):
+    if not re.search("^[456]\d{3}(-?\d{4}){3}$",card) or re.search(r"(\d)\1{3}", re.sub("-", "",card)):
+        return False
+    return True
+
+for i in range(int(input())):
+    print("Valid" if check(input()) else "Invalid")
+
